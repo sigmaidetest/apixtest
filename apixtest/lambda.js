e8882bccbf6cb35b98b021fddce65a2a793678e9
sigmaidetest <cehib@balanc3r.com>
2019-01-20 21:12:54 +0530

    test

---
+++ apixtest/lambda.js
@@ -0,0 +1,26 @@
+
+let Swagger = require('swagger-client');
+
+exports.handler = function (event, context, callback) {
+
+    Swagger.http({
+        url: 'https://services.apixplatform.com/api-sandbox/application/token',
+        method: 'post',
+        query: {},
+        headers: { "Accept": "*/*", "Content-Type": "application/json" },
+        body: JSON.stringify({
+            "userName": "Your email address",
+            "password": "Your password"
+        })
+    }).then((response) => {
+        var access_token = "bearer " + response.body.access_token;
+        // Insert new API calls here to call with APIX Access Token
+
+
+        callback(null, access_token);
+    }).catch((err) => {
+        console.log(err);
+        callback("Execution failed");
+    });
+
+}
